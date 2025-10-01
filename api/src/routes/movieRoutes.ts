d3edf6dbaa7efd1ae503cc32e0b7758bd6722db5
idongesit Samuel Robson <robsonidongesitsamuel@gmail.com>
2025-10-01 09:13:31 +0100

    post error

--- api/src/routes/movieRoutes.ts
+++ api/src/routes/movieRoutes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/add-movie",authenticate,controller.addMovies)
 router.get("/:movieId/single",controller.singleMovies)
 router.get("/all-movie",authenticate,controller.allMovies)
-router.post("/:movieId/update",authenticate,authorize(["ADMIN"]),controller.movieUpdate)
+router.put("/:movieId/update",authenticate,authorize(["ADMIN"]),controller.movieUpdate)
 router.delete("/:id", authenticate,authorize,controller.movieDeleted);
 router.get("/search",authenticate,authorize(["ADMIN"]),controller.moviesSearch);
 
