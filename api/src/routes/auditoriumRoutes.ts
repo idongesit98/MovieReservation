435ed82c0659488ae3d4511a2f3082d788ffa1a8
idongesit Samuel Robson <robsonidongesitsamuel@gmail.com>
2025-10-01 07:49:28 +0100

    middlewares

--- api/src/routes/auditoriumRoutes.ts
+++ api/src/routes/auditoriumRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import * as controller from '../controllers/auditoriumController'
 import { authenticate,authorize } from "../middleware/authMiddleware";
+import { createAuditoriumSchema, validate } from "../middleware/Validations";
 
 const router = express.Router();
 
-//The availability routes and update needs to be checked. for availability when showtime id is created for update individial updated should be allowed
-router.post("/:theatreId/add",authenticate,authorize(["ADMIN"]),controller.addAuditorium)
+router.post("/:theatreId/add",authenticate,authorize(["ADMIN"]),validate(createAuditoriumSchema),controller.addAuditorium)
 router.get("/:theatreId/by-theatre",authenticate,controller.auditoriumByTheatre)
 router.get("/:auditoriumId/:showtimeId/avail",authenticate,controller.availability)
 router.get("/admin-report/:theatreId",authenticate,authorize(["ADMIN"]),controller.adminReports)
--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import * as controller from '../controllers/authController'
+import { loginSchema, signUpSchema, validate } from "../middleware/Validations";
 
 const router = express.Router();
 
 router.post('/login',controller.login)
-router.post('/register-user',controller.registerUser)
-router.post('/logout',controller.logoutUser)
+router.post('/register-user',validate(signUpSchema),controller.registerUser)
+router.post('/logout',validate(loginSchema),controller.logoutUser)
 
 export default router
